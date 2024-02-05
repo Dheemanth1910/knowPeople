b29c2d7bf52867d9721d17e3f14f29a3b60b7ff6
Dheemanth1910 <dheemanth1910@gmail.com>
2024-02-05 12:43:29 +0530

    added passport for cookies and user session

--- routes/home.js
+++ routes/home.js
@@ -1,32 +1,27 @@
 var express = require('express');
+const session = require("express-session");
+const bcrypt = require('bcrypt');
+const passport = require("passport");
+
 const peopleDetailsModel = require("../models/peopleDetails");
 const loginDetailsModel = require("../models/loginDetails");
 
 var router = express.Router();
 router.use(express.urlencoded({ extended: false }));
 
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+
+router.get('/',(req,res)=>{
+  if(req.isAuthenticated()){
+    res.render('home');
+  }
+  
+  else 
+    res.redirect('/')
+})
+
 
 router.post('/',async function(req,res){
-    const password = req.body.password 
-
-    await loginDetailsModel.findOne({email : req.body.username})
-    .then((result) =>{
-      if(result === null){
-        res.send("The entered username doesn't exist Please try again , ");
-      }
-      else{
-         bcrypt.compare(password, result.password).then(function(reslt) {
-            if(reslt){
-              res.render('home');
-            }
-            else{
-              res.send("wrong Password entered, Please try again.");
-            }
-          });
-      }
-    })
+    
 })
 
 
