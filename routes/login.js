19a7876692a3bfc2c2faa445a06fe55e2c28f59b
Dheemanth1910 <dheemanth1910@gmail.com>
2024-02-05 14:55:20 +0530

    added login-page

---
+++ routes/login.js
@@ -0,0 +1,38 @@
+require("../database/connection");
+
+var express = require('express');
+const session = require("express-session");
+const bcrypt = require('bcrypt');
+const passport = require("passport");
+
+
+const peopleDetailsModel = require("../models/peopleDetails");
+
+
+var router = express.Router();
+router.use(express.urlencoded({ extended: false }));
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/',  passport.authenticate("local", {
+  successRedirect: "/home",
+  failureRedirect: "/",
+  failureFlash: true
+}))
+
+
+
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
+
+
+module.exports = router;
