c416185901699221c1cb613d64d6ddfd5ba216d3
Dheemanth1910 <dheemanth1910@gmail.com>
2024-02-12 21:45:00 +0530

    added forgot password functionality using nodemailer

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,40 @@ require("../database/connection");
 var express = require('express');
 const session = require("express-session");
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const loginDetailsModel = require("../models/loginDetails");
 const passport = require("passport");
+const nodemailer = require("nodemailer");
+const {username,pwd} = require('../credentials/gmail.cred');
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: username,
+    pass: pwd,
+  },
+});
+
+function sendMail(email){
+  const otp = Math.floor(Math.random() * 10000);
+  async function main(otp) {  
+    const info = await transporter.sendMail({
+      from: username, 
+      to: email, 
+      subject: "Password Change OTP", 
+      text: `The OTP for changing your password is : ${otp}`, 
+    },(err,info)=>{
+      if(err){
+        console.log(err);
+        return
+      }
+      console.log("Message sent: %s", info.messageId);
+      
+    });
+  }
+  main(otp).catch(console.error);
+  return otp;
+}
 
 
 const peopleDetailsModel = require("../models/peopleDetails");
@@ -24,8 +57,41 @@ router.post('/',  passport.authenticate("local", {
   failureFlash: true
 }))
 
+router.get('/forgotPassword',(req,res)=>{
+  res.render('forgotPassword')
+})
+let otp;
+let username1 ;
+router.post('/forgotPassword',(req,res)=>{
+  // console.log(req.body);
+  username1 = req.body.email,
+  otp = sendMail(req.body.email)
+  res.send("otp sent successfully");
+})
 
+router.post('/checkOtp',(req,res)=>{
+  console.log(req.body.otpVal);
+  if(otp == req.body.otpVal){
+    res.send("true")
+  }
+  else{
+    res.send("false");
+  }
+})
 
+router.post('/updatePassword',async (req,res)=>{
+  let password = req.body.password;
+  bcrypt.hash(password, saltRounds, async function(err, hash) {
+    if(err){
+        console.log(err);
+        res.send("false")
+    }
+    
+    await loginDetailsModel.updateOne({email:username1},{$set:{password:hash}});
+    
+    res.send("data recieved successfully, Please login agian. ")
+})
+})
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
