25137122458ae5b9593020721378c9ecf3b6d11c
Dheemanth1910 <dheemanth1910@gmail.com>
2024-02-19 11:08:14 +0530

    fixed a bug in register page

--- routes/register.js
+++ routes/register.js
@@ -5,8 +5,7 @@ const saltRounds = 10;
 
 router.use(express.urlencoded({ extended: false }));
 const loginDetailsModel = require("../models/loginDetails");
-const peopleDetailsModel = require("../models/peopleDetails")
-
+const peopleDetailsModel = require("../models/peopleDetails");
 router.get('/', function(req, res, next) {
     res.render('register');
   });
@@ -18,7 +17,8 @@ router.post('/submit',async (req,res)=>{
     .then((result) =>{
         console.log(result);
         if(result !== null){
-            res.send("Entered email alrready exists ,, Please try loging in or choose a different email .");
+            // req.flash(messages , "Entered email alrready exists ,, Please try loging in or choose a different email .")
+            res.redirect('/register');
         }
         else{
             bcrypt.hash(myPlaintextPassword, saltRounds, async function(err, hash) {
@@ -29,14 +29,37 @@ router.post('/submit',async (req,res)=>{
                 let data={email : req.body.email, 
                     password : hash,}
                 await loginDetailsModel.create(data);
-                let person = {first_name:req.body.firstName,last_name:req.body.lastName,};
-                await peopleDetailsModel.create(person) ;
-                res.send("data recieved successfully, Please login agian. ")
+                await peopleDetailsModel.aggregate([
+                    {
+                        $group: {
+                            _id: null,
+                            maxNumber: { $max: '$id' } // Replace 'fieldName' with the name of your field
+                        }
+                    }
+                ])
+                .then(async (result)=>{
+                    let uid = result[0].maxNumber +1 ;
+                    let person = {id:uid,
+                        first_name:req.body.firstName,
+                        last_name:req.body.lastName,
+                        email:req.body.email,
+                        gender:req.body.gender,
+                        interests : [],
+                        country:req.body.country,
+                        age:parseInt(req.body.age),
+                        profileImg : "",
+                        bio : "",
+                        phone : 0,
+                        liked : [],
+                        likes :[]
+                        };
+                    await peopleDetailsModel.create(person) ;
+                    res.send("data recieved successfully, Please login agian. ")
+                });
+                
             })
         }
 
     });
-    
 })
-
 module.exports = router;
